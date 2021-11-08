04490234310e1642183479edc5e918edb44651eb
Celeste <Celeste.Lopez@ibm.com>
2021-11-08 13:56:05 +0100

    Changed app styles

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('WELCOME TO CLIENT ENGINEERING APP!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Welcome to IBM Developer Festival!');
   });
 });
